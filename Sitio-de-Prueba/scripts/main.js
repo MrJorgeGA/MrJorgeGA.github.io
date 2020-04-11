117000779434dc9ad38a633951010e15b6dbe0aa
MrJorgeGA <63504993+MrJorgeGA@users.noreply.github.com>
2020-04-11 11:36:17 -0300

    Update main.js

--- Sitio-de-Prueba/scripts/main.js
+++ Sitio-de-Prueba/scripts/main.js
@@ -14,7 +14,7 @@ var miTitulo = document.querySelector('h1');
 
 function estableceNombreUsuario(){
     var miNombre = prompt('Por favor, ingrese su nombre.');
-    if (!miNombre || miNombre === 'miNombre') {
+    if (!miNombre || miNombre === null) {
         estableceNombreUsuario();
     } else {
     localStorage.setItem('nombre', 'miNombre');
